3d74d4002cbcbf693b2ee73b04d2e2c13cd82e16
mys <miccelinski@gmail.com>
2018-08-21 22:18:57 +0200

    moving physics

--- src/js/game.js
+++ src/js/game.js
@@ -2,16 +2,65 @@ kontra.init();
 
 const WALLS_WIDTH = 64
 const BLOCK = 16
+const VELOCITY = 4
+const DECELERATION = 0.1
 let sprites = [];
 
 let player = kontra.sprite({
 	x: 320 / 2 - 8,
 	y: 240 / 2 - 8,
-	dy: 2,
-	width: 16,
-	height: 16,
+	ddy: DECELERATION,
+	width: BLOCK,
+	height: BLOCK,
 	color: 'red',
-	type: 'player'
+	type: 'player',
+	onPlatform: false,
+
+	update(){
+		if (kontra.keys.pressed('up') || kontra.keys.pressed('space')){
+			if (player.dy == 0){
+				this.dy = -(1 + Math.abs(this.dx));
+				this.ddy = DECELERATION;
+				this.onPlatform = false;
+			}
+		}
+		if (kontra.keys.pressed('left') || kontra.keys.pressed('right')){
+			if (kontra.keys.pressed('left')){
+				if (this.dx > 0){
+					this.dx = 0;
+				}
+				this.dx -= 0.1;
+				this.dx = Math.max(this.dx, -VELOCITY);
+			}
+			if (kontra.keys.pressed('right')){
+				if (this.dx < 0){
+					this.dx = 0;
+				}
+				this.dx += 0.1;
+				this.dx = Math.min(this.dx, VELOCITY);
+			}
+		}
+		else {
+			this.dx *= 0.9;
+		}
+
+		// fall from platform
+		for (let i = 0; i < sprites.length; i++){
+			if (this.onPlatform &&
+				sprites[i].type === 'platform' &&
+				(player.x + player.width <= sprites[i].x ||
+				player.x >= sprites[i].x + sprites[i].width))
+			{
+				player.ddy = DECELERATION;
+				onPlatform = false;
+				break;
+			}
+		};
+		this.advance();
+
+		// max speed of fall
+		this.dy = Math.min(this.dy, VELOCITY);
+	}
 })
 
 function createWall(side='left'){
@@ -21,7 +70,8 @@ function createWall(side='left'){
 		x: side == 'left' ? 0 : kontra.canvas.width - WALLS_WIDTH,
 		y: 0,
 		color: 'grey',
-		type: 'wall'
+		type: 'wall',
+		side: side
 	});
 	sprites.push(wall);
 }
@@ -30,7 +80,7 @@ function createPlatform(){
 	let width = Math.floor((Math.random() * 3 + 1)) * 32;
 	let platform = kontra.sprite({
 		width: width,
-		height: 16,
+		height: BLOCK,
 		x: WALLS_WIDTH + Math.floor(Math.random() * (kontra.canvas.width - width- 2 * WALLS_WIDTH)),
 		y: 192,
 		color: 'green',
@@ -43,8 +93,6 @@ createWall('left');
 createWall('right');
 createPlatform();
 
-console.log(sprites);
-
 let loop = kontra.gameLoop({
 	update: function(){
 		sprites.map(sprite => sprite.update());
@@ -52,23 +100,29 @@ let loop = kontra.gameLoop({
 
 		// collide with ground
 		if (player.dy > 0 &&
-			player.y + player.height >= kontra.canvas.height)
+			Math.floor(player.y + player.height) >= kontra.canvas.height)
 		{
-			player.dy = 0;
+			player.y = kontra.canvas.height - player.height;
+			player.dy = player.ddy = 0;
+			player.onPlatform = true;
 		}
 
 		// collide with platforms
-		for (let i = 0; i < sprites.length; i++){
-			if (player.dy > 0 && 
-				sprites[i].type === 'platform' && 
-				player.y + player.height >= sprites[i].y &&
-				player.x + player.width > sprites[i].x &&
-				player.x < sprites[i].x + sprites[i].width)
-			{
-				player.dy = 0;
-				break;
+		if (player.dy > 0){
+			for (let i = 0; i < sprites.length; i++){
+				if (sprites[i].type === 'platform' &&
+					player.y + player.height >= sprites[i].y &&
+					player.y + player.height <= sprites[i].y + 4 &&
+					player.x + player.width > sprites[i].x &&
+					player.x < sprites[i].x + sprites[i].width)
+				{
+					player.y = sprites[i].y - player.height;
+					player.dy = player.ddy = 0;
+					player.onPlatform = true;
+					break;
+				}
 			}
-		};
+		}
 	},
 	render: function(){
 		sprites.map(sprite => sprite.render());
@@ -76,3 +130,5 @@ let loop = kontra.gameLoop({
 	}
 });
 loop.start();
+
+console.log(sprites);
